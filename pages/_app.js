db064674fb17d4c7969a690a993058567db37086
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-07-09 14:15:52 -0400

    initital commit]

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { ChakraProvider, extendTheme, CSSReset } from "@chakra-ui/react"
+import Layout from "../components/Layout"
 import '../styles/globals.css'
 
+const theme = extendTheme({})
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
