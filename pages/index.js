9819d09dc3cfd8446808c02b8a0556654fee6068
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-07-12 00:45:18 -0400

    max width change

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
           Get started by reviewing climbing gyms
         </Text>
 
-        <Flex flexWrap="wrap" margin="auto" alignItems="center" justifyContent="center" maxW="900px" mt="10">
+        <Flex flexWrap="wrap" margin="auto" alignItems="center" justifyContent="center" maxW="920px" mt="10">
           <Box borderColor="teal.300" as="a" href="https://nextjs.org/docs" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={["auto", "45%"]}>
             <Heading as="h3" size="lg" mb="2">Documentation &rarr;</Heading>
             <Text fontSize="lg">Find in-depth information about Next.js features and API.</Text>
